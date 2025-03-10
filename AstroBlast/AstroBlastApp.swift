c9be49616070d1bae2349549f7d2e2bde8f348e7
Marc Fernández <marcausente@gmail.com>
2025-03-10 15:21:57 +0100

    FIRST COMMIT

---
+++ AstroBlast/AstroBlastApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroBlastApp.swift
+//  AstroBlast
+//
+//  Created by Marc Fernández on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AstroBlastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstroBlastTests/AstroBlastTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstroBlastTests.swift
+//  AstroBlastTests
+//
+//  Created by Marc Fernández on 10/3/25.
+//
+
+import Testing
+@testable import AstroBlast
+
+struct AstroBlastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
